b5a5f9b9ba2d87f9bb76f0fc71ac3f72e707f5ba
NguyenGiaHuy-Qi <trueperfect302@gmail.com>
2024-12-12 15:28:47 +0700

    Update bai 7 session 3.c

--- bai 7 session 3.c
+++ bai 7 session 3.c
@@ -3,10 +3,13 @@ int main(){
 	int a,sum = 0;
 	printf("Nhap so nguyen co 4 so: ");
 	scanf("%d",&a);
-	while(a!= 0){
-	int x = a % 10;
-	sum += x;
-	a = a/10;	
-	}printf("vay tong la %d",sum);
+	sum += a % 10;      
+    a /= 10;            
+    sum += a % 10;      
+    a /= 10;            
+    sum += a % 10;     
+    a /= 10;           
+    sum += a;
+    printf("%d",sum);
 	return 0;
 }
